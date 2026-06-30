ff089522c0bef98e05f8da7baac478dae093925d
ukwally <ukwally22rgmail.com>
2026-06-30 14:31:27 +0100

    ...

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const { cache } = require("react");
-
 const CACHE_NAME = 'versao-1';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/manifest.json'
+    './',
+    './index.html',
+    './manifest.json'
 ];
 
 //instala o service worker e guarda os arquivos principais no cache
@@ -12,13 +10,12 @@ self.addEventListener('install',
     (e) => {
         e.waitUntil(
             cache.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-        )
-    })
-//serve os ar   quivos do cache quando o usuario esta offline
+        );
+    });
 
+//serve os ar   quivos do cache quando o usuario esta offline
 self.addEventListener('fetch', (e) => {
     e.respondWith(
         caches.match(e.request).then((response) => response || fetch(e.request))
     );
-
 });
